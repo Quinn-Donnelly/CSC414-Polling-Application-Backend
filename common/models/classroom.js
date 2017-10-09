007d7042be9fb70b33053d42ad0a8f63dc02ad7e
Quintin Donnelly <quinndonnelly22@gmail.com>
2017-10-08 19:12:41 -0500

    Adde the acl to the classroom and the teacher id to be added before creating the classroom

--- common/models/classroom.js
+++ common/models/classroom.js
@@ -2,5 +2,8 @@
 
 // TODO: Add the teacherId for the class
 module.exports = function(Classroom) {
-
+  Classroom.beforeRemote('create', (context, classroom, next) => {
+    context.args.data.teacherId = context.req.accessToken.userId;
+    next();
+  });
 };
